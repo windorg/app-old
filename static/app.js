6a60c54176b5629636165e08ab654b69778b5a86
Artyom Kazak <artyom@artyom.me>
2021-09-11 21:01:31 +0200

    Open external links in a new tab

--- static/app.js
+++ static/app.js
@@ -118,6 +118,18 @@ function onReadyOrTurbo() {
         $(el).addClass('tiptap-processed');
         $(el).hide();
     });
+
+    // Make all external links open in a new tab
+    $('a').each(function() {
+        var a = new RegExp('/' + window.location.host + '/');
+        if(!a.test(this.href)) {
+            $(this).click(function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                window.open(this.href, '_blank');
+            });
+        }
+    });
 };
 
 $(document).ready(function () {
