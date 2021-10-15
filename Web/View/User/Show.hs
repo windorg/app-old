4c3711274a97645dda1b414085553e721b0a9cd6
Artyom Kazak <artyom@artyom.me>
2021-10-15 18:52:06 +0200

    Breadcrumb

--- Web/View/User/Show.hs
+++ Web/View/User/Show.hs
@@ -12,7 +12,8 @@ instance View ShowView where
     html ShowView { .. } = [hsx|
         <nav>
             <ol class="breadcrumb">
-                <li class="breadcrumb-item active">{get #displayName user}</li>
+                <li class="breadcrumb-item"><a href={BoardsAction}>Boards</a></li>
+                <li class="breadcrumb-item active">belonging to @{get #handle user}</li>
             </ol>
         </nav>
         <h1>
